865ae71340e4b7564f1b40bda0edbbb09005f499
saqib-devops <saqibahmad778866@gmail.com>
2023-01-19 03:29:03 -0500

    Rest Auth: version conflict with django new version >> exarth rest auth library

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+from src.api.views import FaceBookLogin
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 # CORE URLS
 urlpatterns = [
 
@@ -40,4 +54,16 @@ urlpatterns += [
     path('500/', TemplateView.as_view(template_name='500.html')),
     # REMOVE THIS WHEN HOME VIEW CREATED
     path('', TemplateView.as_view(template_name='000.html')),
+]
+
+
+# Swagger and Exarth Rest Auth Urls
+urlpatterns +=[
+
+    path('rest-auth/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    re_path('rest-auth/registration/', include('exarth_rest_auth.registration.urls')),
+    re_path('rest-auth/', include('exarth_rest_auth.urls')),
+    re_path('rest-auth/facebook/$', FaceBookLogin.as_view(), name='fb_login'),
 ]
