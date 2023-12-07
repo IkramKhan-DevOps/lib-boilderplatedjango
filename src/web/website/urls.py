2caa11ac27b79ad98c652ca9855b8006cdae37c6
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2023-12-07 23:30:43 +0500

    FEAT: some unknown changes added

--- src/web/website/urls.py
+++ src/web/website/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+
+from .views import (
+    HomeView
+)
 
 app_name = "website"
 urlpatterns = [
-
+    # path('', HomeView.as_view(), name='home')
 ]
--- src/web/website/views.py
+++ src/web/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'website/index.html'
+
+
