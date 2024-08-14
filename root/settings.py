57bd3f8a64a38abe9ce29ecd9693a2ea717c9410
hesham <gwtw.mhn@gmail.com>
2024-08-14 21:17:20 +0500

    FIX: Callback urls

--- root/settings.py
+++ root/settings.py
@@ -23,6 +23,7 @@ CSRF_TRUSTED_ORIGINS = [f'{PROTOCOL}://{host}' for host in ALLOWED_HOSTS]
 LOGOUT_REDIRECT_URL = '/accounts/cross-auth/'
 LOGIN_REDIRECT_URL = '/accounts/cross-auth/'
 GOOGLE_CALLBACK_ADDRESS = f"{BASE_URL}/accounts/google/login/callback/"
+APPLE_CALLBACK_ADDRESS = f"{BASE_URL}/apple/google/login/callback/"
 
 ROOT_URLCONF = 'root.urls'
 AUTH_USER_MODEL = 'users.User'
--- src/api/views.py
+++ src/api/views.py
@@ -2,29 +2,26 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.apple.views import AppleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView, SocialConnectView
-from root.settings import DOMAIN
-
-GOOGLE_CALLBACK_ADDRESS = "accounts/google/login/callback/"
-APPLE_CALLBACK_ADDRESS = "accounts/apple/login/callback/"
+from root.settings import  GOOGLE_CALLBACK_ADDRESS, APPLE_CALLBACK_ADDRESS
 
 
-class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
+class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = f'{DOMAIN}/{GOOGLE_CALLBACK_ADDRESS}'
+    callback_url = GOOGLE_CALLBACK_ADDRESS
     client_class = OAuth2Client
 
 class GoogleConnect(SocialConnectView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = f'{DOMAIN}/{GOOGLE_CALLBACK_ADDRESS}'
+    callback_url = GOOGLE_CALLBACK_ADDRESS
     client_class = OAuth2Client
 
 class AppleLogin(SocialLoginView):
     adapter_class = AppleOAuth2Adapter
-    callback_url = f'{DOMAIN}/{APPLE_CALLBACK_ADDRESS}'
+    callback_url = APPLE_CALLBACK_ADDRESS
     client_class = OAuth2Client
 
 class AppleConnect(SocialConnectView):
     adapter_class = AppleOAuth2Adapter
-    callback_url = f'{DOMAIN}/{APPLE_CALLBACK_ADDRESS}'
+    callback_url = APPLE_CALLBACK_ADDRESS
     client_class = OAuth2Client
 
